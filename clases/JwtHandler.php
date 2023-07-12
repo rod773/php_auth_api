8f331d9c1fff8bab10b93199e8b71e39501e3685
rod773 <104315743+rod773@users.noreply.github.com>
2023-07-12 11:09:16 +0200

    function jwt encode data

--- clases/JwtHandler.php
+++ clases/JwtHandler.php
@@ -1,6 +1,43 @@
 <?php
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 class JwtHandler
 {
+
+    protected $jwt_secret;
+    protected $token;
+    protected $issuedAt;
+    protected $expire;
+    protected $jwt;
+
+
+    public function __construct()
+    {
+        date_default_timezone_set("Europe/Madrid");
+
+        $this->issuedAt = time();
+
+        $this->expire = $this->issuedAt + 3600;
+
+        $this->jwt_secret = $_ENV['JWT_SECRET'];
+    }
+
+
+    public function jwtEncodeData($iss, $data)
+    {
+
+        $this->token = [
+            "iss" => $iss,
+            "aud" => $iss,
+            "iat" => $this->issuedAt,
+            "exp" => $this->expire,
+            "data" => $data
+        ];
+
+        $this->jwt = JWT::encode($this->token, $this->jwt_secret, 'HS256');
+
+        return $this->jwt;
+    }
 }
