8aa6e9eccf4b52b8a4394f55da9772b1a26dbc02
rod773 <104315743+rod773@users.noreply.github.com>
2023-07-12 09:33:25 +0200

    create db connection

--- clases/Database.php
+++ clases/Database.php
@@ -3,4 +3,33 @@
 
 class Database
 {
+
+    private $db_host = $_ENV['DB_HOST'];
+    private $db_name = $_ENV['DB_NAME'];
+    private $db_user = $_ENV['DB_USER'];
+    private $db_pass = $_ENV['DB_PASS'];
+
+
+
+    public function dbConnection()
+    {
+
+        try {
+
+            $conn = new PDO(
+                "mysql:host=" . $this->db_host .
+                    ';db_name=' . $this->db_name,
+                $this->db_user,
+                $this->db_pass
+            );
+
+
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            return $conn;
+        } catch (PDOException $e) {
+            echo  "Connection Error " . $e->getMessage();
+            exit;
+        }
+    }
 }
